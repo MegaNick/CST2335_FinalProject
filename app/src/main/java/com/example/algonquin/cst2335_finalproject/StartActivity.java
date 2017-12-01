3910974c63f75d7a7f41c18be0445a6e8ac5d599
Nick <meln0025@algonquinlive.com>
2017-12-01 14:45:59 -0500

    Initial commit

--- app/src/main/java/com/example/algonquin/cst2335_finalproject/StartActivity.java
+++ app/src/main/java/com/example/algonquin/cst2335_finalproject/StartActivity.java
@@ -2,12 +2,40 @@ package com.example.algonquin.cst2335_finalproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class StartActivity extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+    }
+    @Override
+    public void onPause(){
+        super.onPause();
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+    }
+
+
 }
