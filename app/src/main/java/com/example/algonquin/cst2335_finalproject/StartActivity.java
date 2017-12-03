675152fff115a47d43f1579cf3cbec1c907db5ed
Nick <meln0025@algonquinlive.com>
2017-12-03 00:10:26 -0500

    Initial commit

--- app/src/main/java/com/example/algonquin/cst2335_finalproject/StartActivity.java
+++ app/src/main/java/com/example/algonquin/cst2335_finalproject/StartActivity.java
@@ -1,16 +1,30 @@
 package com.example.algonquin.cst2335_finalproject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends Activity {
+    Button buttonThermostat;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        buttonThermostat = findViewById(R.id.thermostatButton);
+
+        //Start Thermostat Activity
+        buttonThermostat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ThermostatActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
