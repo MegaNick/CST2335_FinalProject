391c2b2f3be72549691fc8075fb1b16c739fa13c
Chen <Jeckychen1989@gmai.com>
2017-12-06 19:03:56 -0500

    Milestone1

--- app/src/main/java/com/example/algonquin/cst2335_finalproject/StartActivity.java
+++ app/src/main/java/com/example/algonquin/cst2335_finalproject/StartActivity.java
@@ -1,16 +1,29 @@
 package com.example.algonquin.cst2335_finalproject;
 
 import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends Activity {
 
+    private Button foodButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        foodButton = (Button)findViewById(R.id.button2);
+        foodButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StartActivity.this, FoodList.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
