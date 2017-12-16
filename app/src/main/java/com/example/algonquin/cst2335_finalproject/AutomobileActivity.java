03a198cf3ae0cef568d2334ea62d301a856e614f
Nick <meln0025@algonquinlive.com>
2017-12-16 11:12:40 -0500

    Milestone 1.9 Commit Helps done just needs testing

--- app/src/main/java/com/example/algonquin/cst2335_finalproject/AutomobileActivity.java
+++ app/src/main/java/com/example/algonquin/cst2335_finalproject/AutomobileActivity.java
@@ -4,14 +4,12 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
